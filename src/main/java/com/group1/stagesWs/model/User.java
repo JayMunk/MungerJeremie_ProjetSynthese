7f75b63461dece2f866a3870d6a39e16a3a97f3f
Jeremie Munger <jaymunger10@live.ca>
2022-02-07 17:38:41 -0500

    Creation model User

---
+++ src/main/java/com/group1/stagesWs/model/User.java
@@ -0,0 +1,26 @@
+package com.group1.stagesWs.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String courriel;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String courriel, String password) {
+        this.courriel = courriel;
+        this.password = password;
+    }
+}
